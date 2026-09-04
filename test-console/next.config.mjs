b7d337f0fc1daab678923b9636328ea5e412caaa
Hanif Imaduddin <78628504+Hanif-Imaduddin@users.noreply.github.com>
2026-09-05 06:57:50 +0700

    delete test-console

--- test-console/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
-  serverExternalPackages: ["pg", "ioredis"],
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
-};
-
-export default nextConfig;
